b629d984be1c494cab4ec2af5e82f74f3da6f9c0
laohanme <jimocheng97@hotmail.com>
2019-01-16 19:37:45 +0800

    Initial Commit

---
+++ Swiftme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftme
+//
+//  Created by Lim Mei Khuan on 16/01/2019.
+//  Copyright © 2019 laohanme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
